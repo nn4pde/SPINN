03e63cf1726b18c04076a0c0141424b1310a4216
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2021-02-16 18:33:51 +0530

    WIP: Reorganize files.
    - Move contents of spinn1d.py and spinn2d.py into oned.py and twod.py.

--- code/oned.py
+++ code/oned.py
@@ -167,6 +167,16 @@ class Problem1D(Problem):
             self.plt1.set_data(xn, pn)
         return self.get_error(xn, pn)
 
+    def plot_weights(self):
+        x = self.nn.centers().detach().cpu().numpy()
+        w = self.nn.weights().detach().cpu().squeeze().numpy()
+        if not self.plt2:
+            self.plt2, = plt.plot(x, np.zeros_like(x), 'o', label='centers')
+            self.plt3, = plt.plot(x, w, 'o', label='weights')
+        else:
+            self.plt2.set_data(x, np.zeros_like(x))
+            self.plt3.set_data(x, w)
+
     def plot(self):
         err = self.plot_solution()
         self.plot_weights()
@@ -198,6 +208,92 @@ class Problem1D(Problem):
             return np.sin(8.0*np.pi*x)/(64.0*np.pi**2)
         elif self.deq == 'simple':
             return 0.5*x*(1.0 - x)
+
+
+class Shift(nn.Module):
+    def __init__(self, points, fixed_points, fixed_h=False):
+        super().__init__()
+        n_free = len(points)
+        n_fixed = len(fixed_points)
+        self.n = n = n_free + n_fixed
+        if n_fixed:
+            xmin = min(np.min(points), np.min(fixed_points))
+            xmax = max(np.max(points), np.max(fixed_points))
+        else:
+            xmin, xmax = np.min(points), np.max(points)
+        dx = (xmax - xmin)/n
+        self.center = nn.Parameter(tensor(points))
+        self.fixed = tensor(fixed_points)
+        if fixed_h:
+            self.h = nn.Parameter(torch.tensor(2.0*dx))
+        else:
+            self.h = nn.Parameter(2.0*dx*torch.ones(n))
+
+    def centers(self):
+        return torch.cat((self.center, self.fixed))
+
+    def forward(self, x):
+        cen = self.centers()
+        return (x - cen)/self.h
+
+
+class SPINN1D(nn.Module):
+    @classmethod
+    def from_args(cls, domain, activation, args):
+        return cls(domain, activation,
+                   fixed_h=args.fixed_h, use_pu=not args.no_pu)
+
+    @classmethod
+    def setup_argparse(cls, parser, **kw):
+        p = parser
+        p.add_argument(
+            '--fixed-h', dest='fixed_h', action='store_true', default=False,
+            help='Use fixed width nodes.'
+        )
+        p.add_argument(
+            '--no-pu', dest='no_pu', action='store_true', default=False,
+            help='Do not use a partition of unity.'
+        )
+
+    def __init__(self, domain, activation, fixed_h=False, use_pu=True):
+        super().__init__()
+
+        self.fixed_h = fixed_h
+        self.use_pu = use_pu
+        self.layer1 = Shift(domain.nodes(), domain.fixed_nodes(),
+                            fixed_h=fixed_h)
+        n = self.layer1.n
+        self.activation = activation
+        self.layer2 = nn.Linear(n, 1, bias=not use_pu)
+        self.layer2.weight.data.fill_(0.0)
+        if not self.use_pu:
+            self.layer2.bias.data.fill_(0.0)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        y = self.layer1(x)
+        y = self.activation(y)
+        if self.use_pu:
+            y1 = y.sum(axis=1).unsqueeze(1)
+        else:
+            y1 = tensor(1.0)
+        y = self.layer2(y/y1)
+        return y.squeeze()
+
+    def centers(self):
+        return self.layer1.centers()
+
+    def widths(self):
+        return self.layer1.h
+
+    def weights(self):
+        return self.layer2.weight
+
+    def show(self):
+        print("Basis centers: ", self.centers())
+        print("Mesh widths: ", self.layer1.h)
+        print("Nodal weights: ", self.layer2.weight)
+        print("Output bias: ", self.layer2.bias)
 
 
 # Activation functions
@@ -318,3 +414,11 @@ class App1D:
 
         if args.directory is not None:
             solver.save()
+
+
+if __name__ == '__main__':
+    app = App1D(
+        problem_cls=Problem1D, nn_cls=SPINN1D,
+        domain_cls=RegularDomain
+    )
+    app.run(nodes=20, samples=80, lr=1e-2)
--- code/twod.py
+++ code/twod.py
@@ -299,6 +299,60 @@ class Shift2D(nn.Module):
         return xh, yh
 
 
+class SPINN2D(nn.Module):
+    @classmethod
+    def from_args(cls, domain, activation, args):
+        return cls(domain, activation,
+                   fixed_h=args.fixed_h, use_pu=not args.no_pu)
+
+    @classmethod
+    def setup_argparse(cls, parser, **kw):
+        p = parser
+        p.add_argument(
+            '--fixed-h', dest='fixed_h', action='store_true', default=False,
+            help='Use fixed width nodes.'
+        )
+        p.add_argument(
+            '--no-pu', dest='no_pu', action='store_true', default=False,
+            help='Do not use a partition of unity.'
+        )
+
+    def __init__(self, domain, activation, n_outputs=1,
+                 fixed_h=False, use_pu=True):
+        super().__init__()
+
+        self.activation = activation
+        self.use_pu = use_pu
+        self.layer1 = Shift2D(domain.nodes(), domain.fixed_nodes(),
+                              fixed_h=fixed_h)
+        n = self.layer1.n
+        self.layer2 = nn.Linear(n, n_outputs, bias=not use_pu)
+        self.layer2.weight.data.fill_(0.0)
+        if not self.use_pu:
+            self.layer2.bias.data.fill_(0.0)
+
+    def forward(self, x, y):
+        x = x.unsqueeze(1)
+        y = y.unsqueeze(1)
+        xh, yh = self.layer1(x, y)
+        z = self.activation(xh, yh)
+        if self.use_pu:
+            zsum = z.sum(axis=1).unsqueeze(1)
+        else:
+            zsum = 1.0
+        z = self.layer2(z/zsum)
+        return z.squeeze()
+
+    def centers(self):
+        return self.layer1.centers()
+
+    def widths(self):
+        return self.layer1.widths()
+
+    def weights(self):
+        return self.layer2.weight
+
+
 def gaussian(x, y):
     return torch.exp(-(x*x + y*y))
 
@@ -363,3 +417,11 @@ class App2D(App1D):
             'kernel': Kernel
         }
         return activations[args.activation](args.kernel_size)
+
+
+if __name__ == '__main__':
+    app = App2D(
+        problem_cls=Problem2D, nn_cls=SPINN2D,
+        domain_cls=RegularDomain
+    )
+    app.run(nodes=40, samples=120, lr=1e-2)
