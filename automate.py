74cb7b38576f46cc22c315e3104a8deaa8651a58
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2021-02-22 21:11:51 +0530

    Add lid-driven-cavity example and update automation.

--- automate.py
+++ automate.py
@@ -11,9 +11,17 @@ matplotlib.use('pdf')
 import matplotlib.pyplot as plt
 from mayavi import mlab
 
+
 fontsize=32
 font = {'size': fontsize}
 matplotlib.rc('font', **font)
+
+
+def figure():
+    plt.figure(figsize=(12, 12))
+    font = {'size': 24}
+    matplotlib.rc('font', **font)
+
 
 def _plot_1d(problem):
     problem.make_output_dir()
@@ -39,10 +47,10 @@ def _plot_1d(problem):
 
         cen = [0.0] + nn_state['layer1.center'].tolist() + [1.0]
         plt.plot(
-            cen, np.zeros_like(cen), 
+            cen, np.zeros_like(cen),
             'bo', markersize=8, label='Nodes'
         )
-        
+
         plt.tight_layout()
         plt.savefig(problem.output_path('n_%d.pdf' % case.params['nodes']))
         plt.close()
@@ -153,10 +161,10 @@ def _plot_ode_conv(problem, n_nodes):
         ## Plot nodal positions
         cen = [0.0] + nn_state['layer1.center'].tolist() + [1.0]
         plt.plot(
-            cen, np.zeros_like(cen), 
+            cen, np.zeros_like(cen),
             'bo', markersize=8, label='Nodes'
         )
-        
+
         plt.tight_layout()
         plt.savefig(problem.output_path(f"{case.params['activation']}_n_{n_nodes}.pdf"))
         plt.close()
@@ -174,7 +182,7 @@ def _plot_ode_conv(problem, n_nodes):
 
     for i in range(len(acts)):
         plt.plot(L1s[i], '-', linewidth=4, label=f'{acts[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_1$ error')
     plt.legend()
@@ -189,7 +197,7 @@ def _plot_ode_conv(problem, n_nodes):
 
     for i in range(len(acts)):
         plt.plot(L2s[i], '-', linewidth=4, label=f'{acts[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_2$ error')
     plt.legend()
@@ -204,7 +212,7 @@ def _plot_ode_conv(problem, n_nodes):
 
     for i in range(len(acts)):
         plt.plot(Linfs[i], '-', linewidth=4, label=f'{acts[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_{\infty}$ error')
     plt.legend()
@@ -248,7 +256,7 @@ class ODE3Conv3(Problem):
     def setup(self):
         self.n = 3
         self.ns = 10*self.n
-        
+
         base_cmd = (
             'python3 code/ode3.py -d $output_dir'
         )
@@ -304,10 +312,10 @@ def _plot_ode_conv_sampling(problem, n_nodes):
         ## Plot nodal positions
         cen = [0.0] + nn_state['layer1.center'].tolist() + [1.0]
         plt.plot(
-            cen, np.zeros_like(cen), 
+            cen, np.zeros_like(cen),
             'bo', markersize=8, label='Nodes'
         )
-        
+
         plt.tight_layout()
         plt.savefig(problem.output_path(f"n_{n_nodes}_f_{case.params['sample_frac']}.pdf"))
         plt.close()
@@ -325,7 +333,7 @@ def _plot_ode_conv_sampling(problem, n_nodes):
 
     for i in range(len(fs)):
         plt.plot(L1s[i], '-', linewidth=4, label=f'f={fs[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_1$ error')
     plt.legend()
@@ -340,7 +348,7 @@ def _plot_ode_conv_sampling(problem, n_nodes):
 
     for i in range(len(fs)):
         plt.plot(L2s[i], '-', linewidth=4, label=f'f={fs[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_2$ error')
     plt.legend()
@@ -355,7 +363,7 @@ def _plot_ode_conv_sampling(problem, n_nodes):
 
     for i in range(len(fs)):
         plt.plot(Linfs[i], '-', linewidth=4, label=f'f={fs[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_{\infty}$ error')
     plt.legend()
@@ -371,7 +379,7 @@ class ODE2Conv6(Problem):
     def setup(self):
         self.n = 6
         self.ns = 20*self.n
-        
+
         base_cmd = (
             'python3 code/ode2.py -d $output_dir'
         )
@@ -414,7 +422,7 @@ def _plot_pde_conv(problem, n_nodes):
 
         mlab.figure(size=(700, 700), bgcolor=(1,1,1), fgcolor=(0,0,0))
         m_plt = mlab.surf(x, y, u, opacity=1.0)
-        mlab.surf(x, y, u_exact, representation='wireframe')    
+        mlab.surf(x, y, u_exact, representation='wireframe')
         mlab.colorbar(m_plt)
         mlab.savefig(problem.output_path(f"sine2d_{case.params['activation']}_n_{n_nodes}.pdf"))
         mlab.close()
@@ -432,7 +440,7 @@ def _plot_pde_conv(problem, n_nodes):
 
     for i in range(len(acts)):
         plt.plot(L1s[i], '-', linewidth=4, label=f'{acts[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_1$ error')
     plt.legend()
@@ -447,7 +455,7 @@ def _plot_pde_conv(problem, n_nodes):
 
     for i in range(len(acts)):
         plt.plot(L2s[i], '-', linewidth=4, label=f'{acts[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_2$ error')
     plt.legend()
@@ -462,7 +470,7 @@ def _plot_pde_conv(problem, n_nodes):
 
     for i in range(len(acts)):
         plt.plot(Linfs[i], '-', linewidth=4, label=f'{acts[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_{\infty}$ error')
     plt.legend()
@@ -521,7 +529,7 @@ def _plot_pde_conv_nodes(problem):
 
         mlab.figure(size=(700, 700), bgcolor=(1,1,1), fgcolor=(0,0,0))
         m_plt = mlab.surf(x, y, u, opacity=1.0)
-        mlab.surf(x, y, u_exact, representation='wireframe')    
+        mlab.surf(x, y, u_exact, representation='wireframe')
         mlab.colorbar(m_plt)
         mlab.savefig(problem.output_path(f"sine2d_n_{case.params['nodes']}.pdf"))
         mlab.close()
@@ -539,7 +547,7 @@ def _plot_pde_conv_nodes(problem):
 
     for i in range(len(n_nodes)):
         plt.plot(L1s[i], '-', linewidth=4, label=f'n~{n_nodes[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_1$ error')
     plt.legend()
@@ -554,7 +562,7 @@ def _plot_pde_conv_nodes(problem):
 
     for i in range(len(n_nodes)):
         plt.plot(L2s[i], '-', linewidth=4, label=f'n~{n_nodes[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_2$ error')
     plt.legend()
@@ -569,7 +577,7 @@ def _plot_pde_conv_nodes(problem):
 
     for i in range(len(n_nodes)):
         plt.plot(Linfs[i], '-', linewidth=4, label=f'n~{n_nodes[i]}')
-        
+
     plt.xlabel('Iterations')
     plt.ylabel(r'$L_{\infty}$ error')
     plt.legend()
@@ -626,7 +634,7 @@ def _plot_pde_conv_nodes_fem(problem):
     plt.figure(figsize=(12,12))
     plt.xscale('log')
     plt.yscale('log')
-    plt.plot(n_nodes, L1s, 'b-',linewidth=6) 
+    plt.plot(n_nodes, L1s, 'b-',linewidth=6)
     plt.xlabel('Number of nodes')
     plt.ylabel(r'$L_1$ error')
     plt.tight_layout()
@@ -637,7 +645,7 @@ def _plot_pde_conv_nodes_fem(problem):
     plt.figure(figsize=(12,12))
     plt.xscale('log')
     plt.yscale('log')
-    plt.plot(n_nodes, L2s, 'b-',linewidth=6) 
+    plt.plot(n_nodes, L2s, 'b-',linewidth=6)
     plt.xlabel('Number of nodes')
     plt.ylabel(r'$L_2$ error')
     plt.tight_layout()
@@ -648,7 +656,7 @@ def _plot_pde_conv_nodes_fem(problem):
     plt.figure(figsize=(12,12))
     plt.xscale('log')
     plt.yscale('log')
-    plt.plot(n_nodes, Linfs, 'b-',linewidth=6) 
+    plt.plot(n_nodes, Linfs, 'b-',linewidth=6)
     plt.xlabel('Number of nodes')
     plt.ylabel(r'$L_{\infty}$ error')
     plt.tight_layout()
@@ -681,8 +689,90 @@ class SquareSlit(Problem):
         _plot_pde_conv_nodes_fem(self)
 
 
-## Irregular domain
-## python poisson2d_irreg_dom.py --plot --lr 1e-3 --gpu
+# Irregular domain
+# python poisson2d_irreg_dom.py --plot --lr 1e-3 --gpu
+
+
+class Cavity(Problem):
+    def get_name(self):
+        return 'cavity'
+
+    def _n_nodes(self, nn_state):
+        return len(nn_state['layer1.x'])
+
+    def setup(self):
+        base_cmd = (
+            'python3 code/cavity.py -d $output_dir '
+        )
+
+        self.cases = [
+            Simulation(
+                root=self.input_path(f'n{i}'),
+                base_command=base_cmd,
+                nodes=i, samples=5000,
+                n_train=10000,
+                lr=5e-4,
+                tol=1e-3,
+                sample_frac=0.1,
+            )
+            for i in (100, 200, 300)
+        ]
+
+    def get_ghia_data(self):
+        fname = os.path.join('code', 'data', 'ghia_re_100.txt')
+        x, v, y, u = np.loadtxt(fname, unpack=True)
+        return x, v, y, u
+
+    def plot_comparison(self):
+        xg, vg, yg, ug = self.get_ghia_data()
+        figure()
+        s1 = plt.subplot(211)
+        s1.plot(ug, yg, 'o', color='red', label='Ghia et al.')
+        s2 = plt.subplot(212)
+        s2.plot(xg, vg, 'o', color='red', label='Ghia et al.')
+        ls = [':', '--', '-']
+        for i, case in enumerate(self.cases):
+            nn_state = torch.load(case.input_path('model.pt'))
+            nodes = self._n_nodes(nn_state)
+            res = np.load(case.input_path('results.npz'))
+            xc, uc, vc = res['xc'], res['uc'], res['vc']
+            s1.plot(uc, xc, ls[i], lw=3,
+                    label='SPINN (%d nodes)' % nodes)
+            s2.plot(xc, vc, ls[i], lw=3,
+                    label='SPINN (%d nodes)' % nodes)
+
+        s1.set_xlabel(r'$u$')
+        s1.set_ylabel(r'$y$')
+        s1.legend()
+        s2.set_xlabel(r'$x$')
+        s2.set_ylabel(r'$v$')
+        s2.legend()
+        fname = self.output_path('centerline_compare.pdf')
+        plt.savefig(fname)
+        plt.close()
+
+    def plot_result(self):
+        for case in self.cases:
+            nn_state = torch.load(case.input_path('model.pt'))
+            nodes = self._n_nodes(nn_state)
+            res = np.load(case.input_path('results.npz'))
+            x, y, U = res['x'], res['y'], res['u']
+            u, v = U[..., 0], U[..., 1]
+            vmag = np.sqrt(u*u + v*v)
+            figure()
+            plt.contourf(x, y, vmag, levels=25)
+            plt.colorbar()
+            plt.xlabel(r'$x$')
+            plt.ylabel(r'$y$')
+            plt.axis('equal')
+            fname = self.output_path('vmag_n%d.pdf' % (nodes))
+            plt.savefig(fname)
+            plt.close()
+
+    def run(self):
+        self.make_output_dir()
+        self.plot_result()
+        self.plot_comparison()
 
 
 if __name__ == '__main__':
@@ -692,7 +782,8 @@ if __name__ == '__main__':
         ODE2Conv6,
         Poisson2DSineConv,
         Poisson2DSineNodes,
-        SquareSlit
+        SquareSlit,
+        Cavity
     ]
     automator = Automator(
         simulation_dir='outputs',
---
+++ code/cavity.py
@@ -0,0 +1,200 @@
+import os
+from mayavi import mlab
+import numpy as np
+import torch
+import torch.autograd as ag
+
+from common import tensor
+from spinn2d import Plotter2D, App2D, SPINN2D
+from pde2d_base import RegularPDE
+
+
+class CavityPDE(RegularPDE):
+    def __init__(self, n_nodes, ns, nb=None, nbs=None, sample_frac=1.0):
+        self.sample_frac = sample_frac
+
+        # Interior nodes
+        n = round(np.sqrt(n_nodes) + 0.49)
+        self.n = n
+        dxb2 = 1.0/(n + 1)
+        xl, xr = dxb2, 1.0 - dxb2
+        sl = slice(xl, xr, n*1j)
+        x, y = np.mgrid[sl, sl]
+        self.i_nodes = (x.ravel(), y.ravel())
+
+        # Fixed nodes
+        nb = n if nb is None else nb
+        self.nb = nb
+        dxb2 = 1.0/(nb)
+        _x = np.linspace(dxb2, 1.0 - dxb2, nb)
+        _o = np.ones_like(_x)
+        x = np.hstack((_x, _o, _x, 0.0*_o))
+        y = np.hstack((_o*0.0, _x, _o, _x))
+        self.f_nodes = (x, y)
+
+        # Interior samples
+        self.ns = ns = round(np.sqrt(ns) + 0.49)
+        dxb2 = 1.0/(ns)
+        xl, xr = dxb2, 1.0 - dxb2
+        sl = slice(xl, xr, ns*1j)
+        x, y = np.mgrid[sl, sl]
+        xs, ys = (tensor(t.ravel(), requires_grad=True)
+                  for t in (x, y))
+        self.p_samples = (xs, ys)
+
+        # Boundary samples
+        nbs = ns if nbs is None else nbs
+        self.nbs = nbs
+        sl = slice(0, 1.0, nbs*1j)
+        _x = np.linspace(dxb2, 1.0 - dxb2, nb)
+        o = np.ones_like(_x)
+
+        def tg(x):
+            return tensor(x, requires_grad=True)
+
+        self.left = left = (tg(0.0*o), tg(_x))
+        self.right = right = (tg(o), tg(_x))
+        self.bottom = bottom = (tg(_x), tg(o)*0.0)
+        self.top = top = (tg(_x), tg(o))
+        self.b_samples = (
+            torch.cat([x[0] for x in (top, bottom, left, right)]),
+            torch.cat([x[1] for x in (top, bottom, left, right)])
+        )
+
+        self.n_interior = len(self.p_samples[0])
+        self.rng_interior = np.arange(self.n_interior)
+        self.sample_size = int(self.sample_frac*self.n_interior)
+
+    def _compute_gradient(self, u, xs, ys):
+        du = ag.grad(
+            outputs=u, inputs=(xs, ys), grad_outputs=torch.ones_like(u),
+            retain_graph=True, create_graph=True
+        )
+        return du[0], du[1]
+
+    def n_vars(self):
+        return 3
+
+    def has_exact(self):
+        return False
+
+    def interior_loss(self, nn):
+        xs, ys = self.interior()
+        U = nn(xs, ys)
+        u = U[:, 0]
+        v = U[:, 1]
+        p = U[:, 2]
+        u, ux, uy, uxx, uyy = self._compute_derivatives(u, xs, ys)
+        v, vx, vy, vxx, vyy = self._compute_derivatives(v, xs, ys)
+        px, py = self._compute_gradient(p, xs, ys)
+
+        # The NS equations.
+        nu = 0.01
+        ce = ((ux + vy)**2).sum()
+        mex = ((u*ux + v*uy + px - nu*(uxx + uyy))**2).sum()
+        mey = ((u*vx + v*vy + py - nu*(vxx + vyy))**2).sum()
+
+        return ce + (mex + mey)
+
+    def boundary_loss(self, nn):
+        bc_weight = self.ns*20
+        xb, yb = self.boundary()
+        Ub = nn(xb, yb)
+        ub = Ub[:, 0]
+        vb = Ub[:, 1]
+        pb = Ub[:, 2]
+        pbx, pby = self._compute_gradient(pb, xb, yb)
+
+        n_top = len(self.top[0])
+        ub_top = ub[:n_top]
+        bc_loss = ((ub_top - 1.0)**2).sum()
+        bc_loss += (ub[n_top:]**2).sum()*bc_weight
+        bc_loss += ((vb)**2).sum()*bc_weight
+        bc_loss += (pby[:2*n_top]**2).sum()
+        bc_loss += (pbx[2*n_top:]**2).sum()
+        return bc_loss
+
+    def plot_points(self):
+        n = min(self.ns*2, 200)
+        x, y = np.mgrid[0:1:n*1j, 0:1:n*1j]
+        return x, y
+
+
+class CavityPlotter(Plotter2D):
+
+    def get_plot_data(self):
+        x, y = self.pde.plot_points()
+        xt, yt = tensor(x.ravel()), tensor(y.ravel())
+        pn = self.nn(xt, yt).detach().cpu().numpy()
+        pn.shape = x.shape + (3,)
+        return x, y, pn
+
+    def plot_weights(self):
+        '''Implement this method to plot any weights.
+        Note this is always called *after* plot_solution.
+        '''
+        nn = self.nn
+        x, y = nn.centers()
+        h = nn.widths().detach().cpu().numpy()
+        x = x.detach().cpu().numpy()
+        y = y.detach().cpu().numpy()
+        if not self.plt2:
+            self.plt2 = mlab.points3d(
+                x, y, np.zeros_like(x), h, mode='2dcircle',
+                scale_factor=1.0, opacity=0.25
+            )
+            self.plt2.glyph.glyph_source.glyph_source.resolution = 20
+        else:
+            self.plt2.mlab_source.trait_set(x=x, y=y, scalars=h)
+
+    def plot_solution(self):
+        xn, yn, pn = self.get_plot_data()
+        u, v, p = pn[..., 0], pn[..., 1], pn[..., 2]
+        for arr in (xn, yn, u, v, p):
+            arr.shape = arr.shape + (1, )
+        vmag = np.sqrt(u*u + v*v)
+        pde = self.pde
+        if self.plt1 is None:
+            mlab.figure(size=(700, 700))
+            if self.show_exact and pde.has_exact():
+                un = pde.exact(xn, yn)
+                mlab.surf(xn, yn, un, representation='wireframe')
+            src = mlab.pipeline.vector_field(
+                xn, yn, np.zeros_like(xn), u, v, np.zeros_like(u),
+                scalars=vmag, name='vectors'
+            )
+            self.plt1 = mlab.pipeline.vectors(
+                src, scale_factor=0.2, mask_points=3
+            )
+            self.plt1.scene.z_plus_view()
+            cgp = mlab.pipeline.contour_grid_plane(
+                src, opacity=0.8
+            )
+            cgp.enable_contours = False
+            mlab.colorbar(self.plt1)
+        else:
+            self.plt1.mlab_source.trait_set(u=u, v=v, scalars=vmag)
+        return self.get_error(xn, yn, pn)
+
+    def save(self, dirname):
+        modelfname = os.path.join(dirname, 'model.pt')
+        torch.save(self.nn.state_dict(), modelfname)
+        rfile = os.path.join(dirname, 'results.npz')
+        x, y, u = self.get_plot_data()
+        lc = tensor(np.linspace(0, 1, 100))
+        midc = tensor(0.5*np.ones(100))
+        xc = torch.cat((lc, midc))
+        yc = torch.cat((midc, lc))
+        data = self.nn(xc, yc).detach().cpu().numpy()
+        vc = data[:, 1][:100]
+        uc = data[:, 0][100:]
+
+        np.savez(rfile, x=x, y=y, u=u, xc=lc, uc=uc, vc=vc)
+
+
+if __name__ == '__main__':
+    app = App2D(
+        pde_cls=CavityPDE, nn_cls=SPINN2D,
+        plotter_cls=CavityPlotter,
+    )
+    app.run(nodes=100, samples=2500, sample_frac=0.1, lr=5e-4, n_train=5000)
